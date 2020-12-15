3754b2387be9b7d3ffca2cd1df8d2a75bd36fc79
Cesare Valitutto <cesare@everledger.io>
2020-12-15 15:26:52 +0000

    Init runner for creating aliases

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -1,16 +1,32 @@
 #!/usr/bin/env bash
 
+ALIASES=("fabkit" "fk")
+
+__add_aliases() {
+    local shell="${1}"
+    local cmd="\n# Fabkit aliases to run commands with ease\n"
+    local to_add=false
+
+    for alias in "${ALIASES[@]}"; do
+        if [[ ! $(cat ~/.${shell}rc | grep "alias ${alias}") ]]; then
+            cmd+="alias ${alias}=./fabkit\n"
+            to_add=true
+        fi
+    done
+
+    if [ "${to_add}" == "true" ]; then
+        echo -e ${cmd} >> ~/.${shell}rc
+        source ~/.${shell}rc 2>/dev/null
+    fi
+}
+
 case $SHELL in
 *bash)
-    if [[ ! $(cat ~/.bashrc | grep fabkit) ]]; then
-        echo "alias fabkit=./fabkit" >>~/.bashrc
-        source ~/.bashrc
-    fi
+    __add_aliases bash
     ;;
 *zsh)
-    if [[ ! $(cat ~/.zshrc | grep fabkit) ]]; then
-        echo "alias fabkit=./fabkit" >>~/.zshrc
-        source ~/.zshrc
-    fi
+    __add_aliases zsh
     ;;
 esac
+
+source ${ROOT}/scripts/autocomplete.sh
